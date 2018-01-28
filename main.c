7cd1f4ffd2648820467f87aee41f58aa03500859
bsse0939 <35795854+bsse0939@users.noreply.github.com>
2018-01-28 15:04:21 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,r;
+
+    printf("Enter your first number:\n");
+    scanf("%d",&x);
+
+    printf("Enter your second number:\n");
+    scanf("%d",&y);
+
+    r=x+y;
+
+    printf("Sum is :%d\n",r);
+
+    return 0;
+ }
